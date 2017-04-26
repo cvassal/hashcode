6485071f2a8347874c010caf753ab74e4960031f
ygrenzinger <yannick.grenzinger@gmail.com>
2017-04-26 14:38:55 +0200

    Array of arrays

---
+++ src/main/java/com/carbonit/hashcode/domain/Buiding.java
@@ -0,0 +1,11 @@
+package com.carbonit.hashcode.domain;
+
+public class Buiding {
+
+    public final Cell t[][];
+
+    public Buiding(int w, int h) {
+        t = new Cell[w][h];
+    }
+
+}
---
+++ src/main/java/com/carbonit/hashcode/domain/Wireless.java
@@ -0,0 +1,11 @@
+package com.carbonit.hashcode.domain;
+
+public class Wireless {
+
+    public final Cell t[][];
+
+    public Wireless(int w, int h) {
+        t = new Cell[w][h];
+    }
+
+}
