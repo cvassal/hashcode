1069ac3be4dd1a1b1ed1ea95a6a639d7fd7b08ff
Sekou Diarra <dev.diarra@gmail.com>
2017-04-26 16:11:32 +0200

    add hashcode to position

--- src/main/java/com/carbonit/hashcode/domain/Position.java
+++ src/main/java/com/carbonit/hashcode/domain/Position.java
@@ -2,13 +2,29 @@ package com.carbonit.hashcode.domain;
 
 public class Position {
 
-    final int rows;
-    final int columns;
+    public final int rows;
+    public final int columns;
 
     public Position(int row, int column) {
         this.rows = row;
         this.columns = column;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Position position = (Position) o;
+
+        if (rows != position.rows) return false;
+        return columns == position.columns;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = rows;
+        result = 31 * result + columns;
+        return result;
+    }
 }
